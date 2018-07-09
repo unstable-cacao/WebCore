cc3e60d3f442bec1e13c903f78b750f63e0474af
Marina Sherstuk <marinas@commpeak.com>
2018-07-09 10:16:49 +0300

    Added Cookie unit tests

---
+++ Tests/WebCore/CookieTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace WebCore;
+
+
+use PHPUnit\Framework\TestCase;
+
+
+class CookieTest extends TestCase
+{
+	public function test_expireAt_Int_SetInt()
+	{
+		$cookie = new Cookie();
+		$time = 555;
+		
+		self::assertEquals($time, $cookie->expireAt($time)->Expire);
+	}
+	
+	public function test_expireAt_String_ConvertToInt()
+	{
+		$cookie = new Cookie();
+		$timeString = '2018-01-01';
+		$timeInt = strtotime($timeString);
+		
+		self::assertEquals($timeInt, $cookie->expireAt($timeString)->Expire);
+	}
+	
+	public function test_create_ReturnCreatedCookie()
+	{
+		$cookieName = 'Test';
+		$cookie = Cookie::create($cookieName);
+		
+		self::assertInstanceOf(Cookie::class, $cookie);
+		self::assertEquals($cookieName, $cookie->Name);
+	}
+}
