3448e5e73141c3ef3454bc47d3ecd81bc9f5d6cb
Marina Sherstuk <marinas@commpeak.com>
2018-07-12 17:53:31 +0300

    Updated response according to review

--- Source/HTTP/Responses/StandardWebResponse.php
+++ Source/HTTP/Responses/StandardWebResponse.php
@@ -3,8 +3,6 @@ namespace WebCore\HTTP\Responses;
 
 
 use WebCore\Cookie;
-use WebCore\IInput;
-use WebCore\Inputs\FromArray;
 use WebCore\IWebResponse;
 
 
@@ -17,13 +15,11 @@ class StandardWebResponse implements IWebResponse
 	
 	private $body 		= '';
 	
+	/** @var callable|null */
+	private $callback 	= null;
 	
-	public function getHeaders(): IInput
-	{
-		return new FromArray($this->headers);
-	}
 	
-	public function getHeadersArray(): array
+	public function getHeaders(): array
 	{
 		return $this->headers;
 	}
@@ -33,11 +29,12 @@ class StandardWebResponse implements IWebResponse
 		$this->headers = $headers;
 	}
 	
-	/**
-	 * @param string $header
-	 * @param string|int $value
-	 */
-	public function setHeader(string $header, $value): void
+	public function addHeaders(array $headers): void
+	{
+		$this->headers = array_merge($this->headers, $headers);
+	}
+	
+	public function setHeader(string $header, string $value): void
 	{
 		$this->headers[$header] = $value;
 	}
@@ -47,12 +44,7 @@ class StandardWebResponse implements IWebResponse
 		return isset($this->headers[$header]);
 	}
 	
-	public function getCookies(): IInput
-	{
-		return new FromArray($this->cookies);
-	}
-	
-	public function getCookiesArray(): array
+	public function getCookies(): array
 	{
 		return $this->cookies;
 	}
@@ -62,11 +54,12 @@ class StandardWebResponse implements IWebResponse
 		$this->cookies = $cookies;
 	}
 	
-	/**
-	 * @param string $cookie
-	 * @param string|int $value
-	 */
-	public function setCookieByName(string $cookie, $value): void
+	public function addCookies(array $cookies): void
+	{
+		$this->cookies = array_merge($this->cookies, $cookies);
+	}
+	
+	public function setCookieByName(string $cookie, string $value): void
 	{
 		$this->cookies[$cookie] = Cookie::create($cookie, (string)$value);
 	}
@@ -76,6 +69,27 @@ class StandardWebResponse implements IWebResponse
 		$this->cookies[$cookie->Name] = $cookie;
 	}
 	
+	/**
+	 * @param string $name
+	 * @param null|string $value
+	 * @param int|string $expire
+	 * @param null|string $path
+	 * @param null|string $domain
+	 * @param bool $secure
+	 * @param bool $serverOnly
+	 */
+	public function createCookie(
+		string $name,
+		?string $value = null,
+		$expire = 0,
+		?string $path = null,
+		?string $domain = null,
+		bool $secure = false,
+		bool $serverOnly = false): void
+	{
+		$this->setCookie(Cookie::create($name, $value, $expire, $path, $domain, $secure, $serverOnly));
+	}
+	
 	public function hasCookie(string $cookie): bool
 	{
 		return isset($this->cookies[$cookie]);
@@ -83,6 +97,12 @@ class StandardWebResponse implements IWebResponse
 	
 	public function getBody(): string
 	{
+		if ($this->callback)
+		{
+			$callback = $this->callback;
+			return $callback();
+		}
+		
 		return $this->body;
 	}
 	
@@ -91,8 +111,31 @@ class StandardWebResponse implements IWebResponse
 		$this->body = $body;
 	}
 	
-	public function setJSON(array $params): void
+	public function setBodyCallback(callable $callback): void
 	{
-		$this->body = jsonencode($params);
+		$this->callback = $callback;
+	}
+	
+	/**
+	 * @param array|int|double|bool|string $body
+	 */
+	public function setJSON($body): void
+	{
+		$this->body = jsonencode($body);
+	}
+	
+	public function apply(): void
+	{
+		foreach ($this->headers as $headerName => $headerValue)
+		{
+			header("$headerName: $headerValue");
+		}
+		
+		foreach ($this->cookies as $cookie)
+		{
+			$cookie->apply();
+		}
+		
+		echo $this->getBody();
 	}
 }
--- Source/IWebResponse.php
+++ Source/IWebResponse.php
@@ -4,30 +4,40 @@ namespace WebCore;
 
 interface IWebResponse
 {
-	public function getHeaders(): IInput;
-	public function getHeadersArray(): array;
 	public function setHeaders(array $headers): void;
-	
-	/**
-	 * @param string $header
-	 * @param string|int $value
-	 */
-	public function setHeader(string $header, $value): void;
+	public function addHeaders(array $headers): void;
+	public function setHeader(string $header, string $value): void;
 	public function hasHeader(string $header): bool;
 	
-	public function getCookies(): IInput;
-	public function getCookiesArray(): array;
 	public function setCookies(array $cookies): void;
+	public function addCookies(array $cookies): void;
+	public function setCookieByName(string $cookie, string $value): void;
+	public function setCookie(Cookie $cookie): void;
 	
 	/**
-	 * @param string $cookie
-	 * @param string|int $value
+	 * @param string $name
+	 * @param null|string $value
+	 * @param int|string $expire
+	 * @param null|string $path
+	 * @param null|string $domain
+	 * @param bool $secure
+	 * @param bool $serverOnly
 	 */
-	public function setCookieByName(string $cookie, $value): void;
-	public function setCookie(Cookie $cookie): void;
+	public function createCookie(
+		string $name,
+		?string $value = null,
+		$expire = 0,
+		?string $path = null,
+		?string $domain = null,
+		bool $secure = false,
+		bool $serverOnly = false): void;
 	public function hasCookie(string $cookie): bool;
 	
-	public function getBody(): string;
 	public function setBody(string $body): void;
-	public function setJSON(array $params): void;
+	public function setBodyCallback(callable $callback): void;
+	
+	/**
+	 * @param array|int|double|bool|string $body
+	 */
+	public function setJSON($body): void;
 }
