31c08bae0376f76b76bb86dc81a1b0e849f722b5
Marina Sherstuk <marinas@commpeak.com>
2018-12-27 15:48:52 +0200

    Implemented new functions for FromArray

--- Source/Inputs/FromArray.php
+++ Source/Inputs/FromArray.php
@@ -4,6 +4,7 @@ namespace WebCore\Inputs;
 
 use Traitor\TEnum;
 use WebCore\Exception\BadRequestException;
+use WebCore\Exception\ServerErrorException;
 use WebCore\Exception\WebCoreFatalException;
 use WebCore\IInput;
 use WebCore\Inputs\Utils\BooleanConverter;
@@ -14,12 +15,173 @@ class FromArray implements IInput
 {
 	private $source;
 	
+	private $minStrLen;
+	private $maxStrLen;
+	
+	private $min;
+	private $max;
+	private $minInclusive;
+	private $maxInclusive;
+	
+	
+	private function isMatchLength(string $string): bool 
+	{
+		if (is_null($this->minStrLen) || is_null($this->maxStrLen))
+			return true;
+		
+		$len = strlen($string);
+		$result = $len >= $this->minStrLen && $len <= $this->maxStrLen;
+		
+		$this->minStrLen = null;
+		$this->maxStrLen = null;
+		
+		return $result;
+	}
+	
+	/**
+	 * @param int|float $val
+	 * @return bool
+	 */
+	private function isMatchRange($val): bool
+	{
+		$result = true;
+		
+		if (!is_null($this->min))
+			$result = $result && ($val > $this->min);
+		
+		if (!is_null($this->max))
+			$result = $result && ($val < $this->max);
+		
+		if (!is_null($this->minInclusive))
+			$result = $result && ($val >= $this->minInclusive);
+		
+		if (!is_null($this->maxInclusive))
+			$result = $result && ($val <= $this->maxInclusive);
+		
+		$this->min = null;
+		$this->max = null;
+		$this->minInclusive = null;
+		$this->maxInclusive = null;
+		
+		return $result;
+	}
+	
 	
 	public function __construct(array $source)
 	{
 		$this->source = $source;
 	}
 	
+	
+	public function withLength(int $length, ?int $max = null): IInput
+	{
+		if (is_null($max)) 
+		{
+			$max = $length;
+			$length = 0;
+		}
+		
+		if ($max < $length || $max < 0 || $length < 0)
+		{
+			throw new ServerErrorException("Parameters passed to function are not valid: $length, $max");
+		}
+		
+		$this->minStrLen = $length;
+		$this->maxStrLen = $max;
+		
+		return $this;
+	}
+	
+	public function withExactLength(int $length): IInput
+	{
+		if ($length < 0) 
+		{
+			throw new ServerErrorException("Parameters passed to function are not valid: $length");
+		}
+		
+		$this->minStrLen = $length;
+		$this->maxStrLen = $length;
+		
+		return $this;
+	}
+	
+	/**
+	 * @param int|float $a
+	 * @param int|float $b
+	 * @return IInput
+	 */
+	public function between($a, $b): IInput
+	{
+		if (!is_numeric($a))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $a");
+		
+		if (!is_numeric($b))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $b");
+		
+		if ($b < $a)
+			throw new ServerErrorException("Parameter 'b' is bigger than parameter 'a': $a, $b");
+		
+		$this->minInclusive = $a;
+		$this->maxInclusive = $b;
+		
+		return $this;
+	}
+	
+	/**
+	 * @param int|float $a
+	 * @return IInput
+	 */
+	public function greaterThen($a): IInput
+	{
+		if (!is_numeric($a))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $a");
+		
+		$this->min = $a;
+		
+		return $this;
+	}
+	
+	/**
+	 * @param int|float $a
+	 * @return IInput
+	 */
+	public function lessThen($a): IInput
+	{
+		if (!is_numeric($a))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $a");
+		
+		$this->max = $a;
+		
+		return $this;
+	}
+	
+	/**
+	 * @param int|float $a
+	 * @return IInput
+	 */
+	public function greaterOrEqualThen($a): IInput
+	{
+		if (!is_numeric($a))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $a");
+		
+		$this->minInclusive = $a;
+		
+		return $this;
+	}
+	
+	/**
+	 * @param int|float $a
+	 * @return IInput
+	 */
+	public function lessOrEqualThen($a): IInput
+	{
+		if (!is_numeric($a))
+			throw new ServerErrorException("Parameter passed to function must be numeric: $a");
+		
+		$this->maxInclusive = $a;
+		
+		return $this;
+	}
 	
 	public function has(string $name): bool
 	{
@@ -57,7 +219,7 @@ class FromArray implements IInput
 	
 	public function int(string $name, ?int $default = null): ?int
 	{
-		return $this->isInt($name) ? (int)$this->source[$name] : $default;
+		return ($this->isInt($name) && $this->isMatchRange((int)$this->source[$name])) ? (int)$this->source[$name] : $default;
 	}
 	
 	public function bool(string $name, ?bool $default = null): ?bool 
@@ -69,7 +231,7 @@ class FromArray implements IInput
 	
 	public function float(string $name, ?float $default = null): ?float 
 	{
-		return $this->isFloat($name) ? (float)$this->source[$name] : $default;
+		return ($this->isFloat($name) && (float)$this->source[$name]) ? (float)$this->source[$name] : $default;
 	}
 	
 	public function regex(string $name, string $regex, ?string $default = null): ?string
@@ -95,7 +257,9 @@ class FromArray implements IInput
 	
 	public function string(string $name, ?string $default = null): ?string
 	{
-		return $this->has($name) && is_string($this->source[$name]) ? $this->source[$name] : $default;
+		return ($this->has($name) && is_string($this->source[$name]) && $this->isMatchLength($this->source[$name])) ? 
+			$this->source[$name] : 
+			$default;
 	}
 	
 	public function enum(string $name, $enumValues, ?string $default = null): ?string
@@ -126,6 +290,9 @@ class FromArray implements IInput
 		if (!is_string($this->source[$name]))
 			throw new BadRequestException("Required parameter must be string");
 		
+		if (!$this->isMatchLength($this->source[$name]))
+			throw new BadRequestException("Required parameter's length must be between {$this->minStrLen} and {$this->maxStrLen}");
+		
 		return $this->source[$name];
 	}
 	
@@ -136,6 +303,9 @@ class FromArray implements IInput
         
         if (!InputValidationHelper::isInt($this->source[$name]))
 			throw new BadRequestException("Required parameter must be int");
+        
+        if (!$this->isMatchRange((int)$this->source[$name]))
+			throw new BadRequestException("Required parameter is not in required range");
         
         return (int)$this->source[$name];
 	}
@@ -158,6 +328,9 @@ class FromArray implements IInput
 		
 		if (!InputValidationHelper::isFloat($this->source[$name]))
 			throw new BadRequestException("Required parameter must be bool");
+		
+		if (!$this->isMatchRange((float)$this->source[$name]))
+			throw new BadRequestException("Required parameter is not in required range");
         
         return (float)$this->source[$name];
 	}
