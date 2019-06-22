fd6083c663f0cc64466fa8fbd7fd82f719d4637f
Alexey Puchkov <alexey.pkv@gmail.com>
2019-06-22 11:06:13 +0300

    Add shortcut class name for standard web request and standard web response

---
+++ Source/WebRequest.php
@@ -0,0 +1,8 @@
+<?php
+namespace WebCore;
+
+
+use WebCore\HTTP\Requests\StandardWebRequest;
+
+
+class WebRequest extends StandardWebRequest {}
---
+++ Source/WebResponse.php
@@ -0,0 +1,8 @@
+<?php
+namespace WebCore;
+
+
+use WebCore\HTTP\Responses\StandardWebResponse;
+
+
+class WebResponse extends StandardWebResponse {}
